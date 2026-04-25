b556ae2f790d4406d230dad60863371c8b0cd060
Daniel Safikhany <safikhany.daniel@gmail.com>
2026-04-25 20:00:27 +0200

    Add function get_steering_angle_range

--- planner/max_steering_angle.py
+++ planner/max_steering_angle.py
@@ -1,8 +1,10 @@
-from models.types import VehicleParameters
+from models.models import *
 import numpy as np
 import math
+import timeit
 
-def get_max_steering_angle(v: float, max_a_lat: float, params: VehicleParameters) -> float:
+
+def get_max_steering_angle(velocity: float, max_a_lat: float, params: VehicleParameters) -> float:
     """
     Compute the maximum feasible steering angle (in radians) based on a lateral
     acceleration constraint using a simplified kinematic bicycle model.
@@ -32,19 +34,72 @@ def get_max_steering_angle(v: float, max_a_lat: float, params: VehicleParameters
     float
         Maximum steering angle in radians, limited by both the lateral
         acceleration constraint and the vehicle's steering limit.
-
-    Raises
-    ------
-    ValueError
-        If velocity v is zero (division by zero).
     """
-    # Prevent division by zero
-    if v == 0:
-        raise ValueError("Velocity must be non-zero.")
+    # If moving very slowly, the lateral acceleration constraint is irrelevant.
+    if abs(velocity) < 0.1: 
+        return params.max_steer
     
     # Compute steering angle using the rearranged bicycle model formula
-    res =  np.arctan((max_a_lat*params.L)/np.square(v))
+    res =  np.arctan((max_a_lat*params.L)/np.square(velocity))
     return min(res, params.max_steer)
+
+
+def get_steering_angle_range(velocity: float,
+                             steering_angle: float,
+                             vehicle_params: VehicleParameters,
+                             max_a_lat: float,
+                             internal_dt: int) -> tuple[float, float]: 
+    """
+    Compute the feasible steering range based on mechanical limits, 
+    actuator rate, and lateral acceleration safety constraints.
+
+    Parameters
+    ----------
+    velocity : float
+        Current vehicle speed in m/s.
+    steering_angle : float
+        Current Steering angle in radians.
+    vehicle_params : VehicleParameters
+        Vehicle constants (wheelbase, max steer, max steer rate).
+    max_a_lat : float
+        Maximum allowable lateral acceleration in m/s².
+    internal_dt : int
+        Time step duration in milliseconds (ms).
+
+    Returns
+    -------
+    tuple[float, float]
+        Feasible (min_angle, max_angle) in radians.
+    """
+    
+    # 1. Physical Sanity Check
+    if abs(steering_angle) > vehicle_params.max_steer:
+        raise ValueError("Steering Angle exceeds VehicleParameters limits")
+    
+    # 2. Safety & Mechanical: Get max angle allowed by lateral acceleration or physical stop
+    max_steering_angle_global = get_max_steering_angle(velocity, max_a_lat, vehicle_params)
+
+    # 3. Dynamic: Max steering travel possible within the time step (dt)
+    max_steering_delta = vehicle_params.max_steer_rate * (internal_dt / 1000)
+    min_steering_angle_local = steering_angle - max_steering_delta
+    max_steering_angle_local = steering_angle + max_steering_delta
+
+    # 4. Intersection: Limit reachable range by safety and mechanical bounds
+    min_steering_angle = max(min_steering_angle_local, -max_steering_angle_global)
+    max_steering_angle = min(max_steering_angle_local, max_steering_angle_global)
+
+    # 5. Reachability: Ensure min_steering_angle <= max_steering_angle by collapsing the range 
+    # if safety limits force a movement faster than the actuator's rate capacity.
+    if min_steering_angle > max_steering_angle:
+        max_steering_angle = min_steering_angle
+    elif max_steering_angle < min_steering_angle:
+        min_steering_angle = max_steering_angle
+
+    return (min_steering_angle, max_steering_angle)
+
+    
+    
+
 
 
 
@@ -71,9 +126,19 @@ def main():
 
             mu=0.85
         )
+    
+    
+    
+    res = get_steering_angle_range(velocity=4, steering_angle=0.2, vehicle_params=vehicle, max_a_lat=3, internal_dt=100)
+    print(res)
 
-    res = get_max_steering_angle(3, 3, vehicle)
-    print(math.degrees(res))
+
+    res = get_max_steering_angle(velocity=4, max_a_lat=3, params=vehicle)
+    print(res)
+
+    # Benchmarking
+    t  = timeit.timeit(lambda: get_steering_angle_range(velocity=100, steering_angle=0.7, vehicle_params=vehicle, max_a_lat=3, internal_dt=100), number=10000)
+    print(f"Average time: {(t/10000)*1000:.6f} ms")
 
 
 if __name__ == "__main__":
