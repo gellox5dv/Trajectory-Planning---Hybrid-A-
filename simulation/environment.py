520c1d7f0135b5faf7eac963ef6fa6942cb2b211
SaadRasheed-exe <saadrasheed2003@gmail.com>
2026-05-15 14:17:03 +0200

    chore: readjust dimensions

--- simulation/environment.py
+++ simulation/environment.py
@@ -3,7 +3,7 @@ from types import SimpleNamespace
 from models.models import Vector2D, Lane, ObjectType, DynamicObjectStamped, DynamicObject, Environment
 
 
-LANE_DIMS = SimpleNamespace(width = 4.0, length = 8000.0)
+LANE_DIMS = SimpleNamespace(width = 4.0, length = 2000.0)
 VEHICLE_DIMS = SimpleNamespace(width = 2.0, length = 5.0)
 
 
@@ -24,7 +24,7 @@ lane2 = Lane(
 obs1 = DynamicObject(
     id = 1,
     obj_class = ObjectType.VEHICLE,
-    pos = Vector2D(x = 1200.0, y = 2.0),
+    pos = Vector2D(x = 100.0, y = 2.0),
     yaw = 0.0,
     velocity = 8.0 + 1/3,       # 30 km/h
     acceleration = 0.0,
@@ -35,7 +35,7 @@ obs1 = DynamicObject(
 obs2 = DynamicObject(
     id = 2,
     obj_class = ObjectType.VEHICLE,
-    pos = Vector2D(x = 4000.0, y = 6.0),
+    pos = Vector2D(x = 1800.0, y = 6.0),
     yaw = pi,
     velocity = 13.0 + 8/9,      # 50 km/h
     acceleration = 0.0,
